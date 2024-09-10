d30cd8cee4512210dc7028825b7f38de3f4e9904
FarmerL <fludenab@uni.pe>
2024-09-10 11:02:22 -0500

    first commit

---
+++ src/users.ts
@@ -0,0 +1,93 @@
+export const users = [
+    {
+        id: '1',
+        name: 'Emilia Clark',
+        image: 'emilia clark.jpg',
+        role: 'Administradora',
+        email: 'emili.c@gmail.com'
+    },
+    {
+        id: '2',
+        name: 'Karla Sifuentes',
+        image: 'karla sifuentes.jpg',
+        role: 'Asistente contable',
+        email: 'karlita.sif@gmail.com'
+    },
+    {
+        id: '3',
+        name: 'Sergio Guerra',
+        image: 'sergio guerra.jpg',
+        role: 'Programador junior',
+        email: null
+    },
+    {
+        id: '4',
+        name: 'Vero Mendoza',
+        image: 'vero mendoza.jpg',
+        role: '',
+        email: 'v.mendoza@hotmail.com'
+    },
+    {
+        id: '5',
+        name: 'Jorge Negrete',
+        image: '',
+        role: 'Programador Frontend',
+        email: 'negrete@gmail.com'
+    },
+    {
+        id: '6',
+        name: 'Rosa Maldonado',
+        image: 'rosa maldonado.jpg',
+        role: 'Product Owner',
+        email: null
+    },
+    {
+        id: '7',
+        name: 'Carlos Perez',
+        image: 'carlos perez.jpg',
+        role: 'Programador Backend',
+        email: ''
+    },
+    {
+        id: '8',
+        name: 'Luisa Torres',
+        image: 'luisa torres.jpg',
+        role: 'Diseñadora',
+        email: 'luisat@gmail.com'
+    },
+    {
+        id: '9',
+        name: 'Javier Mendoza',
+        image: 'javier mendoza.jpg',
+        role: 'Programador Fullstack',
+        email: 'javi@gmail.com'
+    },
+    {
+        id: '10',
+        name: 'Martha Ruiz',
+        image: 'martha ruiz.jpg',
+        role: 'Programadora',
+        email: ''
+    },
+    {
+        id: '11',
+        name: 'Luis Perez',
+        image: 'luis perez.jpg',
+        role: 'Programador',
+        email: ''
+    },
+    {
+        id: '12',
+        name: 'Ana Torres',
+        image: 'ana torres.jpg',
+        role: 'Diseñadora',
+        email: null
+    },
+    {
+        id: '13',
+        name: 'Carlos Mendoza',
+        image: 'carlos mendoza.jpg',
+        role: 'Programador',
+        email: 'carlos.mendoza@gmail.com'
+    }
+]
